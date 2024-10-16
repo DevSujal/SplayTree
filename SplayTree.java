4a8abad9736640bed9f4fb670fc95c4865161130
DevSujal <nimjesujal2004@gmail.com>
2024-10-16 19:08:17 +0530

    first commit

---
+++ SplayTree.java
@@ -0,0 +1,11 @@
+class SplayTree {
+    // create a binary search tree structure
+    // write function for levelorder traversal
+    //write a function for search in splay tree
+    // write a function for insert in splay tree
+    // write a function for delete in splay tree
+    // write a function for rotate left
+    // write a function for rotate right
+    // write a function for moving a recently used node to root
+    // write a main function to test the above functions
+}
